b0d1dc34d5336d9c42a83ef49a354036d4f9996d
miramiell <kairaku.sakuhin@gmail.com>
2026-05-13 13:38:46 +0700

    More cards and more QoL

--- data.js
+++ data.js
@@ -163,6 +163,278 @@ const cards = [
   },
 
   {
+    type: "major",
+
+    name: "The Lovers",
+    oc: "Seyren Anand",
+
+    image: "images/06_the_lovers.png",
+
+    upright: [
+      "Love",
+      "Romance",
+      "Choice"
+    ],
+
+    reversal: [
+      "Disharmony",
+      "Indifference",
+      "Indecision"
+    ],
+
+    description:
+    " ",
+
+    artist:
+    "© yukicole02"
+  },
+
+  {
+    type: "major",
+
+    name: "The Chariot",
+    oc: "Anleifr Crocus Alcott",
+
+    image: "images/07_the_chariot.png",
+
+    upright: [
+      "Direction",
+      "Control",
+      "Willpower"
+    ],
+
+    reversal: [
+      "Aggression",
+      "Fear of Failure",
+      "Lack of Control"
+    ],
+
+    description:
+    "The rider, one-handedly reining a horse, represents balance of control, even with the handicap of disability—as the crocus flower blooms forward towards the beginning of spring, a new start, bursting colours. March forward, beyond the limitations!",
+
+    artist:
+    "© miramiell"
+  },
+
+  {
+    type: "major",
+
+    name: "Strength",
+    oc: "Lancelot",
+
+    image: "images/08_strength.png",
+
+    upright: [
+      " ",
+      " ",
+      " "
+    ],
+
+    reversal: [
+      " ",
+      " ",
+      " "
+    ],
+
+    description:
+    " ",
+
+    artist:
+    "© yaocchi"
+  },
+
+  {
+    type: "major",
+
+    name: "The Hermit",
+    oc: "Vincent Emerson",
+
+    image: "images/09_the_hermit.png",
+
+    upright: [
+      "Reflection",
+      "Knowledge",
+      "Inner Guidance"
+    ],
+
+    reversal: [
+      "Isolation",
+      "Withdrawal",
+      "Lost Direction"
+    ],
+
+    description: [
+    "Symbolism: Wisteria (Background) -- Wisdom & contemplation / Deer -- Inner guidance & intuition / Wooden Staff -- Support & guidance through one's journey / Cloak -- Solitude & self-reflection / Scroll -- Knowledge & seeking truth"
+    ],
+
+    artist:
+    "© zeonxeria"
+  },
+
+  {
+    type: "major",
+
+    name: "Wheel of Fortune",
+    oc: "Chetandaya Gujarati",
+
+    image: "images/10_wheel_of_fortune.png",
+
+    upright: [
+      "Cycles",
+      "Decisive Moments",
+      "Fortune"
+    ],
+
+    reversal: [
+      "Bad Luck",
+      "Clinging to COntrol",
+      "Unwelcome Changes"
+    ],
+
+    description: [
+      " "
+    ],
+
+    artist: [
+    "© straightyaoi"
+    ],
+  },
+
+  {
+    type: "major",
+
+    name: "Justice",
+    oc: "Maeryn Varaleth",
+
+    image: "images/11_justice.png",
+
+    upright: [
+      "Fairness",
+      "Accountability",
+      "Balance"
+    ],
+
+    reversal: [
+      "Injustice",
+      "Denial",
+      "Imbalance"
+    ],
+
+    description:
+    "Maeryn being open-eyed instead of blindfolded signifies conscious judgment. The sword represents decisive clarity. The scales balance opposing forces: blood and water evoke harm and healing, consequence and mercy, shadow and light. The green surroundings suggest growth through truth, while the gold lines imply divine or higher law.",
+
+    artist:
+    "© artsyjulia"
+  },
+
+  {
+    type: "major",
+
+    name: "The Hanged Man",
+    oc: "Cotton Silke",
+
+    image: "images/12_the_hanged_man.png",
+
+    upright: [
+      "Sacrifice",
+      "Waiting",
+      "Uncertainty"
+    ],
+
+    reversal: [
+      "Stalling",
+      "Disinterest",
+      "Apathy"
+    ],
+
+    description:
+    " ",
+
+    artist:
+    "© accyber"
+  },
+
+  {
+    type: "major",
+
+    name: "Death",
+    oc: "Meena Vaishant",
+
+    image: "images/13_death.png",
+
+    upright: [
+      "Impermanence",
+      "Change",
+      "New Cycle"
+    ],
+
+    reversal: [
+      "Chained",
+      "Not Flowing",
+      "Drowning"
+    ],
+
+    description:
+    "The Death card signifies change, from something old, Aella accepts us in her underwater palace and then she sends us off, lovingly, to something new.",
+
+    artist:
+    "© psyalera"
+  },
+
+  {
+    type: "major",
+
+    name: "Temperance",
+    oc: "Jonah Branford",
+
+    image: "images/14_temperance.png",
+
+    upright: [
+      "Peace",
+      "Patience",
+      "Moderation"
+    ],
+
+    reversal: [
+      "Discord",
+      "Hastiness",
+      "Antagonism"
+    ],
+
+    description:
+    "The temperance card depicts an angel wearing robes with a triangle inside a square, the pouring of liquid between two cups, one foot in the water and one on land, a path leading to a rising sun, a halo, and iris flowers. How I decided to combine the iconic liquid pouring and the project theme was by turning a waterfall into the other cup that poured into Jonah’s chalice.",
+
+    artist:
+    "© sirnix"
+  },
+  {
+    type: "major",
+
+    name: "The Devil",
+    oc: "Lyco Zahrani",
+
+    image: "images/15_the_devil.png",
+
+    upright: [
+      "Obsession",
+      "Dependency",
+      "Limitations"
+    ],
+
+    reversal: [
+      "Freedom",
+      "Revelation",
+      "Reclaiming Power"
+    ],
+    
+    description:
+    " ",
+
+    artist:
+    "© ichigolollipop"
+  },
+
+  {
     type: "minor",
     
     name: "test",
