d0b42c03840f2eb8bddbf5fefc21aa1c681c7287
miramiell <kairaku.sakuhin@gmail.com>
2026-05-13 04:35:22 +0000

    Added 00-05

--- data.js
+++ data.js
@@ -40,14 +40,114 @@ const cards = [
     reversal: [
         "Manipulation",
         "Cunning",
-        "Trickery",
+        "Trickery"
     ],
 
     description:
     "The Magician's card portraits a person whose hands are pointing as arrows up and down, to the skies and to earth. They also have the symbols of the Minor Arcana surrounding them (wand, pentacle, sword, and a cup). A snake and an infinity symbol linger, as signs of unlimited potential and continued cycles. Their red and white robe means purity, inexperience, willpower and passion, also meaning egoism, rage and revenge.",
 
     artist:
-    "© birbankie",
-  }
+    "© birbankie"
+  },
+
+  {
+    name: "The High Priestess",
+    oc: "Vayu Maheswari",
+
+    image: "images/02_the_high_priestess.png",
+
+    upright: [
+      " ",
+      " ",
+      " "
+    ],
+
+    reversal: [
+      " ",
+      " ",
+      " "
+    ],
+
+    description:
+    " ",
+
+    artist:
+    "© spacepolarbear"
+  },
+
+  {
+    name: "The Empress",
+    oc: "Myrh Bakshi",
+
+    image: "images/03_the_empress.png",
+
+    upright: [
+      "Prosperity",
+      "Success",
+      "Growth"
+    ],
+
+    reversal: [
+      "Insecurity",
+      "Self-doubt",
+      "Stagnation"
+    ],
+
+    description:
+    " ",
+
+    artist:
+    "© daylightprint"
+  },
+
+  {
+    name: "The Emperor",
+    oc: "Isarius vur Yvea",
+
+    image: "images/04_the_emperor.png",
+
+    upright: [
+      " ",
+      " ",
+      " "
+    ],
+
+    reversal: [
+      " ",
+      " ",
+      " "
+    ],
+
+    description:
+    " ",
+
+    artist:
+    "© terraru"
+  },
+
+  {
+    name: "The Hierophant",
+    oc: "Constantine",
+
+    image: "images/05_the_hierophant.png",
+
+    upright: [
+      "Wisdom",
+      "Belief",
+      "Stability"
+    ],
+
+    reversal: [
+      "Confusion",
+      "Rebellion",
+      "Restlessness"
+    ],
+
+    description:
+    "This card depicts Constantine as a devoted religious leader, bearing his duty as the servant of God Hiraes and guiding those who seek the Lord’s wisdom. To many, he is the unwavering figure of faith and spirituality, yet beneath the holy robes and silver key stands only a human: a mortal, who bleeds the same red as those who kneel in prayer and desperation.",
+
+    artist:
+    "© yami_izumi"
+  },
 
 ];
